8b7f7ca7b84159fd0ff57cf35bf8ecfab773bffe
Luchesio <oseghaleoyugbo@gmail.com>
2025-07-31 00:38:54 +0100

    routing issue

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,5 +3,5 @@ import { AssessmentComponent } from './assessment/assessment.component';
 export const routes: Routes = [
 
 
-    { path: 'assessment', component: AssessmentComponent },
+    { path: '', component: AssessmentComponent },
 ];
